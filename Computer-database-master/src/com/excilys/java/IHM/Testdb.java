164ba2a088f8ec1f1d94e03d3b78c66cd296af8a
nathmotion <mallet.nathanael@hotmail.fr>
2018-02-15 12:02:45 +0100

    clean du code apres code review

--- Computer-database-master/src/com/excilys/java/IHM/Testdb.java
+++ Computer-database-master/src/com/excilys/java/IHM/Testdb.java
@@ -1,6 +1,7 @@
 package com.excilys.java.IHM;
 
 import java.io.Console;
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -20,109 +21,104 @@ public class Testdb {
 		ServiceCompany servcompany = ServiceCompany.INSTANCE;
 		ServiceComputer servcomputer = ServiceComputer.INSTANCE;
 		String choix ;
-		/*
-		 * 
-		 * 			C L I 
-		 * 
-		 * */
+		
+						//===========	 C L I	===========
 		Scanner sc =  new Scanner(System.in);
-		Console console= System.console();
 		do {	
-			//choix=null;
 			printMenu();
 			choix = sc.nextLine();
-	    
+
 			switch(choix){
 			case "1": 
-				
+
 				ArrayList<Company>  tableCompany= servcompany.getDao().getAll();
 				afficheListCompany(tableCompany);
-				//sc.nextLine();
 				continue;
-			case "2" :
-				// affihce le contenu de list de compagnie
 				
-
+			case "2" :
 				ArrayList<Computer> tableComputer= servcomputer.getDao().getAll();
 				afficheListComputer(tableComputer);
-				
 				continue;
+				
 			case "3" :
 
-				System.out.println("") ;
-				System.out.println("") ;
-				System.out.println("") ;
-				System.out.println("") ;
-				System.out.println("") ;
-				System.out.println("") ;
-				System.out.println("Veullez saisir l' id de l'ordinateur ");
-				System.out.println();
-				int id = sc.nextInt();
 				
-
-				Optional<Computer> opcomputer = servcomputer.getDao().findById(id)	;
-				if(opcomputer.isPresent())
-				{
-					Computer computer = opcomputer.get();
-					System.out.println();	
-					System.out.println("id: "+computer.getId());
-					System.out.println("name: "+computer.getName());
-					System.out.println("introduced: "+computer.getIntroduced());
-					System.out.println("discontinued: "+computer.getDiscontinued());
-					System.out.println("company_id: "+computer.getCompany_id());
-					System.out.println();	
-				}
-				else {
-					//System.out.println(" error aucun computer trouver");
-				}
 				sc.nextLine();
 				continue;
 			}
-		
+
+
+		}while(!choix.equals("exit"));
+
+	}
 	
-	}while(!choix.equals("exit"));
 	
-	//	console.readLine();
-		
-		
-}
 	/**
 	 *  AFFICHE LE MENU 
 	 */
 	public static void printMenu() {
-		System.out.println("") ;
-		System.out.println("") ;
-		System.out.println("") ;
+		
 		System.out.println("1) Afficher listes des compagnies				2) Afficher listes des ordinateurs ");
-		
 		System.out.println("3) Affiches Les detail Ordinateur				4) Ajouter un ordinateur ");
-		
 		System.out.println("5) mise a jour d'un ordinateur					6) Supprimer d'un ordinateur ");
-		System.out.println();
 		System.out.println("Saisir votre choix : ");
 	}
+	
 	/**
 	 * 	AFFICHE DE LA LISTE DES COMPUTER
 	 * @param tableComputer
 	 */
 	public static void afficheListComputer(ArrayList<Computer> tableComputer) {
+		clearConsole();
+
 		tableComputer.forEach((computer ->{
-			System.out.println();	
 			System.out.println("id: "+computer.getId());
 			System.out.println("name: "+computer.getName());
-			System.out.println();	
-			}));
+
+		}));
 	}
+	
 	/**	
 	 * 		AFFICHE LA LISTE DES COMPANY
 	 * @param tableCompany
 	 */
 	public static void afficheListCompany(ArrayList<Company> tableCompany) {
+		clearConsole();
+
 		tableCompany.forEach((company ->{
-			System.out.println();	
 			System.out.println("id: "+company.getId());
 			System.out.println("name: "+company.getName());
-			System.out.println();	
+
 		}));
+
 	}
+	
+	public void afficheFindbyId(Scanner sc, ServiceComputer servcomputer) {
+		System.out.println("Veullez saisir l' id de l'ordinateur ");
+		int id = sc.nextInt();
+
+
+		Optional<Computer> opcomputer = servcomputer.getDao().findById(id)	;
+		if(opcomputer.isPresent())
+		{
+			Computer computer = opcomputer.get();
+			System.out.println("id: "+computer.getId());
+			System.out.println("name: "+computer.getName());
+			System.out.println("introduced: "+computer.getIntroduced());
+			System.out.println("discontinued: "+computer.getDiscontinued());
+			System.out.println("company_id: "+computer.getCompany_id());
+		}
+		else {
+			//System.out.println(" error aucun computer trouver");
+		}
+	}
+	
+	public final static void clearConsole()
+	{
+		for(int clear = 0; clear < 20; clear++)
+		  {
+		     System.out.println() ;
+		  }
+	}
+
 }
--- Computer-database-master/src/com/excilys/java/cdb/model/Computer.java
+++ Computer-database-master/src/com/excilys/java/cdb/model/Computer.java
@@ -74,6 +74,64 @@ public class Computer {
 	public void setDiscontinued(Timestamp discontinuation) {
 		discontinued = discontinuation;
 	}
+
+
+	@Override
+	public String toString() {
+		return "Computer [id=" + id + ", name=" + name + ", introduced=" + introduced + ", discontinued=" + discontinued
+				+ ", company_id=" + company_id + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((company_id == null) ? 0 : company_id.hashCode());
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (company_id == null) {
+			if (other.company_id != null)
+				return false;
+		} else if (!company_id.equals(other.company_id))
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	
 	
 	
