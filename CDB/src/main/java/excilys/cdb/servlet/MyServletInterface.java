9208b6a7ba0084c792ae72fc5d7562b1901cb350
nathmotion <mallet.nathanael@hotmail.fr>
2018-02-23 16:55:02 +0100

    webui: dashboard jsp + css

--- CDB/src/main/java/excilys/cdb/servlet/MyServletInterface.java
+++ CDB/src/main/java/excilys/cdb/servlet/MyServletInterface.java
@@ -11,14 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MyServletInterface
  */
-@WebServlet("/MyServletInterface")
+@WebServlet("/dashboard")
 public class MyServletInterface extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println(" lolllllooll ");
-
+		request.getRequestDispatcher("WEB-INF/views/dashboard.jsp").forward(request, response);
 	}
 
 
